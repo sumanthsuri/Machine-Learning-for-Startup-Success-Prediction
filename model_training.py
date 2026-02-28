166d434d86465934837a71c8a03f01a74261451f
sumanthsuri <sumanthsuri4@gmail.com>
2026-02-28 16:17:30 +0530

    Delete model_training.py

--- model_training.py
+++
@@ -1,9 +0,0 @@
-
-# Machine Learning for Startup Success Prediction
-
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
-print("Project Title Updated Successfully")
